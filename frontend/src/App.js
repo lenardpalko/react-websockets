cf450baea163b657fb8be5e82277be5b71d6df67
Lenard Palko <lenard.palko@gmail.com>
2018-05-26 00:00:52 +0300

    Added react application.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+import Pusher from 'pusher-js';
+import ChatList from './ChatList';
+import ChatBox from './ChatBox';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      username: '',
+      chats: []
+    };
+  }
+
+  componentDidMount() {
+    const username = window.prompt('Username: ', 'Anonymous');
+    this.setState({ username });
+    const pusher = new Pusher('bc3339de2baeab7e11af', {
+      cluster: 'eu',
+      encrypted: true
+    });
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', data => {
+      this.setState({ chats: [...this.state.chats, data], test: '' });
+    });
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(e) {
+    if (e.keyCode === 13) {
+      const payload = {
+        username: this.state.username,
+        message: this.state.text
+      };
+      axios.post('http://localhost:5000/message', payload);
+    } else {
+      this.setState({ text: e.target.value });
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +50,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section>
+          <ChatBox
+            text={this.state.text}
+            username={this.state.username}
+            handleTextChange={this.handleTextChange}
+          />
+          <ChatList chats={this.state.chats} />
+        </section>
       </div>
     );
   }
